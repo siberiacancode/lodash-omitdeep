5baba2559221135eb5158e7aa9ebe329ebd27d2e
hwapedro <dmitriybabinup@gmail.com>
2023-06-20 22:59:39 +0700

    ☄ fix build proccess, delete useless deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 
 const pkg = require('./package.json');
 
@@ -23,10 +22,5 @@ export default [
     ],
     external: Object.keys(pkg.peerDependencies),
     plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.prod.json' }), terser()]
-  },
-  {
-    input: 'src/index.ts',
-    output: [{ file: 'dist/types.d.ts', format: 'es' }],
-    plugins: [dts.default()]
   }
 ];
