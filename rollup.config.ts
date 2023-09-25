6835bef16054170ddffbf20c821da1c75f7930a9
debabin <dmitriybabinup@gmail.com>
2023-09-25 18:18:59 +0700

    ☄ update builder package

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,4 +2,12 @@ import { generateRollupConfig } from '@siberiacancode/builder';
 
 import pkg from './package.json';
 
-export default generateRollupConfig({ pkg });
+export default generateRollupConfig({
+  pkg,
+  configs: {
+    babel: {
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
+    }
+  }
+});
